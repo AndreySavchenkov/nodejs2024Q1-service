983503d9b312bbf90bbfc2c26ed50332fe2a335c
AndreySavchenkov <sannndvik16@gmail.com>
2024-03-04 10:48:29 +0100

    fix: delete test user controller from master branch

--- src/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {
-  @Get()
-  findAll(): string {
-    return 'I showed test users O_o';
-  }
-}
