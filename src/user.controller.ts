c5a05f3e7ee15f41eadbc2cb6890c530eefb277e
AndreySavchenkov <sannndvik16@gmail.com>
2024-03-04 11:50:10 +0100

    feat: create UserModule

--- src/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {
-  @Get()
-  findAll(): string {
-    return 'I am test user controller';
-  }
-}
