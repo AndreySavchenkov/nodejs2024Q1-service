c2f1d3c8d9ab7035bcdd34284027abf3015ee2ca
AndreySavchenkov <sannndvik16@gmail.com>
2024-03-31 12:42:20 +0200

    feat: create logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { LoggerMiddleware } from './utils/logger.middleware';
 
 @Module({
   imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavoriteModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
